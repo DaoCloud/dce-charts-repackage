0c611c3c475d2a0c76660918c9c01c9cfe2cea89
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2022-08-17 21:39:40 +0800

    test
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- test/spiderpool/install.sh
+++ test/spiderpool/install.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+CURRENT_FILENAME=$( basename $0 )
+CURRENT_DIR_PATH=$(cd $(dirname $0); pwd)
+
+CHART_DIR=$1
+KIND_KUBECONFIG=$2
+
+[ -d "$CHART_DIR" ] || { echo "error, failed to find chart $CHART_DIR " ; exit 1 ; }
+[ -f "$KIND_KUBECONFIG" ] || { echo "error, failed to find kubeconfig $KIND_KUBECONFIG " ; exit 1 ; }
+
+echo "CHART_DIR $CHART_DIR"
+echo "KIND_KUBECONFIG $KIND_KUBECONFIG"
