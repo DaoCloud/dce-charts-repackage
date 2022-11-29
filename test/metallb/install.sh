c3ec97962f04e41a507a749162c17df3a427f41d
cyclinder <qifeng.guo@daocloud.io>
2022-11-29 17:07:01 +0800

    bump metallb to 0.13.7 && adjust resources of metallb
    Signed-off-by: cyclinder <qifeng.guo@daocloud.io>

--- test/metallb/install.sh
+++ test/metallb/install.sh
@@ -12,7 +12,7 @@ KIND_KUBECONFIG=$2
 echo "CHART_DIR: $CHART_DIR"
 echo "KIND_KUBECONFIG: $KIND_KUBECONFIG"
 
-HELM_MUST_OPTION=" --timeout 10m0s --wait --debug --kubeconfig ${KIND_KUBECONFIG} "
+HELM_MUST_OPTION=" --timeout 5m0s --wait --debug --kubeconfig ${KIND_KUBECONFIG} "
 
 #==================== add your deploy code bellow =============
 #==================== notice , prometheus CRD has been deployed , so you no need to =============
@@ -20,15 +20,12 @@ HELM_MUST_OPTION=" --timeout 10m0s --wait --debug --kubeconfig ${KIND_KUBECONFIG
 set -x
 
 # deploy the metallb
-helm install metallb ${CHART_DIR}  ${HELM_MUST_OPTION}   --namespace kube-system \
+helm install metallb ${CHART_DIR}  ${HELM_MUST_OPTION}   --namespace metallb-system  --create-namespace \
   --set instances.enabled=true \
-  --set instances.arp.nodeSelectors='{Node1,Node2}' \
-  --set instances.ipAddressPools.addresses="{192.168.1.0/24,172.16.20.1-172.16.20.100,fd00:81::172:81:0:110-fd00:81::172:81:0:120}" \
-  --set metallb.prometheus.podMonitor.enabled=true \
-  --set metallb.prometheus.prometheusRule.enabled=true \
-  --set metallb.prometheus.serviceAccount=insight-agent-kube-prometh-operator \
-  --set metallb.prometheus.namespace=insight-system
-
+  --set instances.arp.nodeSelectors.key=kubernetes.io/arch \
+  --set instances.arp.nodeSelectors.value=amd64 \
+  --set instances.arp.interfaces="{eth0,eth1}" \
+  --set instances.ipAddressPools.addresses="{192.168.1.0/24,172.16.20.1-172.16.20.100,fd00:81::172:81:0:110-fd00:81::172:81:0:120}"
 
 if (($?==0)) ; then
   echo "succeeded to deploy $CHART_DIR"
