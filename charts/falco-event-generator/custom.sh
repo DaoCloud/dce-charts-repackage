56edf53248165f22e378c2aba46a1f93813c416f
weizhoublue <weizhou.lan@daocloud.io>
2022-12-26 13:58:15 +0800

    d
    Signed-off-by: weizhoublue <weizhou.lan@daocloud.io>

--- charts/falco-event-generator/custom.sh
+++ charts/falco-event-generator/custom.sh
@@ -36,7 +36,7 @@ sed -i 's?falcosecurity/event-generator?docker.m.daocloud.io/falcosecurity/event
 #========================= add image registry for relok8s
 
 sed -i '/repository:/ a\\    registry: docker.m.daocloud.io' values.yaml
-sed -i '/repository:/ a\\    registry: docker.m.daocloud.io' charts/event-generator/values.yaml
+sed -i '/repository:/ a\\  registry: docker.m.daocloud.io' charts/event-generator/values.yaml
 sed -iE 's?repository: .*?repository: falcosecurity/event-generator?' values.yaml
 sed -iE 's?repository: .*?repository: falcosecurity/event-generator?' charts/event-generator/values.yaml
 sed -iE 's?image:.*?image: image: "{{ .Values.image.registry }}/{{ .Values.image.repository }}:{{ .Values.image.tag | default .Chart.AppVersion }}"?'  charts/event-generator/templates/pod-template.tpl
