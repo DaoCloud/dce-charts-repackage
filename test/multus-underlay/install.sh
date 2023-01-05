a4fe83fd4e88449dd88a26f26f4aaa25ff779738
cyclinder <qifeng.guo@daocloud.io>
2023-01-05 10:57:39 +0800

    multus: opt namespace hardcode
    Signed-off-by: cyclinder <qifeng.guo@daocloud.io>

--- test/multus-underlay/install.sh
+++ test/multus-underlay/install.sh
@@ -9,14 +9,14 @@ KIND_NAME=$3
 
 [ -d "$CHART_DIR" ] || { echo "error, failed to find chart $CHART_DIR " ; exit 1 ; }
 [ -f "$KIND_KUBECONFIG" ] || { echo "error, failed to find kubeconfig $KIND_KUBECONFIG " ; exit 1 ; }
-[ -z "KIND_NAME" ] || { echo "error, failed to find kind_name $KIND_NAME " ; exit 1 ; }
+[ -n "${KIND_NAME}" ] || { echo "error, failed to find kind_name $KIND_NAME " ; exit 1 ; }
 
 echo "CHART_DIR $CHART_DIR"
 echo "KIND_KUBECONFIG $KIND_KUBECONFIG"
 echo "KIND_NAME: ${KIND_NAME}"
 
 HELM_MUST_OPTION=" --timeout 10m0s --wait --debug --kubeconfig ${KIND_KUBECONFIG} \
---namespace kube-system \
+--namespace kube-public \
 --set sriov.manifests.enable=true \
 --set sriov.sriov_crd.resourceName=intel.com/mlnx_sriov_rdma \
 --set overlay_subnet.service_subnet.ipv6=fed0::1/64  "
@@ -57,10 +57,10 @@ else
   exit 1
 fi
 
-kubectl wait --for=condition=ready -l app=multus --timeout=300s pod -n kube-system --kubeconfig ${KIND_KUBECONFIG}
+kubectl wait --for=condition=ready -l app=multus --timeout=300s pod -n kube-public --kubeconfig ${KIND_KUBECONFIG}
 
 KIND_NODE=`docker ps | grep 'control-plane' | awk '{print $1}'`
 EXIST=`docker exec ${KIND_NODE} ls /opt/cni/bin `
 
-kubectl get po -n kube-system --kubeconfig ${KIND_KUBECONFIG}
+kubectl get po -n kube-public --kubeconfig ${KIND_KUBECONFIG}
 kubectl get network-attachment-definitions.k8s.cni.cncf.io -A --kubeconfig ${KIND_KUBECONFIG}
