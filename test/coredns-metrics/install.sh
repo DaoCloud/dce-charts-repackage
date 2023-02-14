d272f7d3f2c1ff73cdb71a3c1c87e01c467b6b9d
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2023-02-14 20:15:41 +0800

    ingore syncer-chart for coredns
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- test/coredns-metrics/install.sh
+++ test/coredns-metrics/install.sh
@@ -3,29 +3,32 @@
 CURRENT_FILENAME=$( basename "$0" )
 CURRENT_DIR_PATH=$(cd "$CURRENT_FILENAME" || exit; pwd)
 
-KIND_KUBECONFIG=$1
+CHART_DIR=$1
+KIND_KUBECONFIG=$2
 
+[ -d "$CHART_DIR" ] || { echo "error, failed to find chart $CHART_DIR " ; exit 1 ; }
 [ -f "$KIND_KUBECONFIG" ] || { echo "error, failed to find kubeconfig $KIND_KUBECONFIG " ; exit 1 ; }
 
+echo "CHART_DIR: $CHART_DIR"
 echo "KIND_KUBECONFIG: $KIND_KUBECONFIG"
 
-helm repo update chart-museum  --kubeconfig ${KIND_KUBECONFIG}
 HELM_MUST_OPTION=" --timeout 10m0s --wait --debug --kubeconfig ${KIND_KUBECONFIG} "
 
 #==================== add your deploy code bellow =============
 #==================== notice, prometheus CRD has been deployed , so you no need to =============
+# !!!!!!!!! this chart is special, it does not contain any image, which make syncer-chart fail !!!!!!!!!
 
 set -x
 
 # deploy cert-manager
 # shellcheck disable=SC2086
-helm install coredns chart-museum/coredns ${HELM_MUST_OPTION} --set isInstallSM=true --set isInstallPR=true --set isInstallGD=true  --namespace kube-system
+helm install coredns "${CHART_DIR}" ${HELM_MUST_OPTION} --set isInstallSM=true --set isInstallPR=true --set isInstallGD=true  --namespace kube-system
 
 
 if (($?==0)) ; then
-  echo "succeeded to deploy $CHART_DIR"
-  exit 0
+ echo "succeeded to deploy $CHART_DIR"
+ exit 0
 else
-  echo "error, failed to deploy $CHART_DIR"
-  exit 1
+ echo "error, failed to deploy $CHART_DIR"
+ exit 1
 fi
