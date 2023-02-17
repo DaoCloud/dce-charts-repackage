715e61523cd7b996b23be06dde14f1bbb6807d04
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2023-02-17 18:35:02 +0800

    fix falco
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- charts/falco/custom.sh
+++ charts/falco/custom.sh
@@ -17,10 +17,14 @@ set -o nounset
 APP_VERSION=` cat charts/falco/Chart.yaml | grep appVersion | awk '{print $2}'`
 sed -iE "s?tag:.*?tag: ${APP_VERSION}?g" values.yaml
 
-yq -i ' .falco.falcosidekick.image.registry = "docker.m.daocloud.io" ' values.yaml
 yq -i ' .falco.image.registry = "docker.m.daocloud.io" ' values.yaml
 yq -i ' .falco.falco.json_output = true ' values.yaml
 yq -i ' .falco.driver.loader.initContainer.image.registry = "docker.m.daocloud.io" ' values.yaml
+
+KICKAPP_VERSION=` cat charts/falco/charts/falcosidekick/Chart.yaml | grep appVersion | awk '{print $2}'`
+yq -i ' .falco.falcosidekick.image.registry = "docker.m.daocloud.io" ' values.yaml
+yq -i ' .falco.falcosidekick.image.repository = "falcosecurity/falcosidekick" ' values.yaml
+yq -i " .falco.falcosidekick.image.tag = \"${KICKAPP_VERSION}\" " values.yaml
 
 rm -f values.yamlE || true
 rm -rf charts/falco/charts/falcosidekick/templates/tests
