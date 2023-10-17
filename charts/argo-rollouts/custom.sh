f16bc21272ac3d77a300795ed2aa9cd17c7d58ff
xiaowu.zhu <xiaowu.zhu@daocloud.io>
2023-10-17 17:44:50 +0800

    upgrade argo-rollouts to v2.32.0

--- charts/argo-rollouts/custom.sh
+++ charts/argo-rollouts/custom.sh
@@ -27,12 +27,57 @@ if ! which yq &>/dev/null ; then
      mv /tmp/${YQ_BINARY} /usr/bin/yq
 fi
 
-yq -i '
-  .argo-rollouts.controller.image.registry = "quay.m.daocloud.io" |
-  .argo-rollouts.dashboard.image.registry = "quay.m.daocloud.io"
-' values.yaml
+# re-write global config.
+
+defaultVersion=$(yq .appVersion Chart.yaml)
+yq -i "
+  .argo-rollouts.controller.image.registry = \"quay.m.daocloud.io\" |
+  .argo-rollouts.controller.image.tag = \"${defaultVersion}\" |
+  .argo-rollouts.dashboard.image.registry = \"quay.m.daocloud.io\" |
+  .argo-rollouts.dashboard.image.tag = \"${defaultVersion}\" |
+  .argo-rollouts.global.imageRegistry=\"quay.m.daocloud.io\" |
+  .argo-rollouts.global.repository=\"argoproj/argo-rollouts\" |
+  .argo-rollouts.global.tag = \"${defaultVersion}\"
+" values.yaml
 
 #==============================
-echo "fulfill tag"
-sed -i 's@tag: ""@tag: "master"@g' values.yaml
+os=$(uname)
+
+
+echo '
+{{- define "global.images.image" -}}
+{{- $registryName := .imageRoot.registry -}}
+{{- $repositoryName := .imageRoot.repository -}}
+{{- $tag := .imageRoot.tag | toString -}}
+{{- if .global }}
+    {{- if .global.imageRegistry }}
+     {{- $registryName = .global.imageRegistry -}}
+    {{- end -}}
+    {{- if and .global.repository (eq $repositoryName "") }}
+     {{- $repositoryName = .global.repository -}}
+    {{- end -}}
+    {{- if and .global.tag (eq $tag "")}}
+     {{- $tag = .global.tag -}}
+    {{- end -}}
+{{- end -}}
+{{- if .tag }}
+    {{- if .tag.imageTag }}
+     {{- $tag = .tag.imageTag -}}
+    {{- end -}}
+{{- end -}}
+{{- if $registryName }}
+{{- printf "%s/%s:%s" $registryName $repositoryName $tag -}}
+{{- else -}}
+{{- printf "%s:%s" $repositoryName $tag -}}
+{{- end -}}
+{{- end -}}
+'>>charts/argo-rollouts/templates/_helpers.tpl
+
+if [ $os == "Darwin" ];then
+  sed -i "" 's?"{{ .Values.controller.image.registry }}/{{ .Values.controller.image.repository }}:{{ default .Chart.AppVersion .Values.controller.image.tag }}"?{{ include "global.images.image" (dict "imageRoot" .Values.controller.image "global" .Values.global ) }}?g' charts/argo-rollouts/templates/controller/deployment.yaml
+  sed -i "" 's?"{{ .Values.dashboard.image.registry }}/{{ .Values.dashboard.image.repository }}:{{ default .Chart.AppVersion .Values.dashboard.image.tag }}"?{{ include "global.images.image" (dict "imageRoot" .Values.dashboard.image "global" .Values.global ) }}?g' charts/argo-rollouts/templates/dashboard/deployment.yaml
+elif [ $os == "Linux" ];then
+  sed -i 's?"{{ .Values.controller.image.registry }}/{{ .Values.controller.image.repository }}:{{ default .Chart.AppVersion .Values.controller.image.tag }}"?{{ include "global.images.image" (dict "imageRoot" .Values.controller.image "global" .Values.global ) }}?g' charts/argo-rollouts/templates/controller/deployment.yaml
+  sed -i 's?"{{ .Values.dashboard.image.registry }}/{{ .Values.dashboard.image.repository }}:{{ default .Chart.AppVersion .Values.dashboard.image.tag }}"?{{ include "global.images.image" (dict "imageRoot" .Values.dashboard.image "global" .Values.global ) }}?g' charts/argo-rollouts/templates/dashboard/deployment.yaml
+fi
 exit $?
