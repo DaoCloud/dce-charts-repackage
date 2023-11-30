d27d8ae5cf73021ddcd51d6d68bb02dfbdd0657f
ii2day <ji.li@daocloud.io>
2023-11-30 14:33:23 +0800

    bump kdoctor to v0.2.1
    Signed-off-by: ii2day <ji.li@daocloud.io>

--- charts/kdoctor/custom.sh
+++ charts/kdoctor/custom.sh
@@ -7,6 +7,16 @@ cd $CHART_DIRECTORY
 echo "custom shell: CHART_DIRECTORY $CHART_DIRECTORY"
 echo "CHART_DIRECTORY $(ls)"
 
+
+if ! which yq &>/dev/null ; then
+    echo " 'yq' no found, try to install..."
+    YQ_VERSION=v4.30.6
+    YQ_BINARY="yq_$(uname | tr 'A-Z' 'a-z')_amd64"
+    wget https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/${YQ_BINARY}.tar.gz -O /tmp/yq.tar.gz &&
+     tar -xzf /tmp/yq.tar.gz -C /tmp &&
+     mv /tmp/${YQ_BINARY} /usr/bin/yq
+fi
+
 #========================= add your customize bellow ====================
 #===============================
 
@@ -18,8 +28,10 @@ set -o nounset
 APP_VERSION=` cat charts/kdoctor/Chart.yaml | grep appVersion | awk '{print $2}'`
 sed -iE "s?tag:.*?tag: \"v${APP_VERSION}\"?g" values.yaml
 
-yq -i ' .kdoctor.global.imageRegistryOverride = "ghcr.m.daocloud.io" ' values.yaml
-yq -i ' .kdoctor.kdoctorAgent.service.type = "NodePort" ' values.yaml
+yq -i '
+    .kdoctor.global.imageRegistryOverride="ghcr.m.daocloud.io" |
+    .kdoctor.kdoctorAgent.service.type="NodePort"
+' values.yaml
 
 rm -f values.yamlE || true
 
