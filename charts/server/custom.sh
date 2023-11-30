ef595208e84333979c3087cfe1f5af4a60e93d2d
ii2day <ji.li@daocloud.io>
2023-11-30 16:19:58 +0800

    bump kdoctor server to v0.2.1
    Signed-off-by: ii2day <ji.li@daocloud.io>

--- charts/server/custom.sh
+++ charts/server/custom.sh
@@ -18,7 +18,11 @@ set -o nounset
 APP_VERSION=` cat charts/server/Chart.yaml | grep appVersion | awk '{print $2}'`
 sed -iE "s?tag:.*?tag: \"v${APP_VERSION}\"?g" values.yaml
 
-yq -i ' .image.registry = "ghcr.m.daocloud.io" ' values.yaml
+yq -i ' .server.image.registry="ghcr.m.daocloud.io" ' values.yaml
+yq -i ' .server.resources.limits.cpu="500m" ' values.yaml
+yq -i ' .server.resources.requests.cpu="100m" ' values.yaml
+yq -i ' .server.resources.limits.memory="1024Mi" ' values.yaml
+yq -i ' .server.resources.requests.memory="256Mi" ' values.yaml
 
 rm -f values.yamlE || true
 
