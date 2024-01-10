7cc33c30c47a993191a153eb2d76d1386fbe2f48
zhongjun.li <zhongjun.li@daocloud.io>
2024-01-10 11:26:36 +0800

    enhance-prometheus-adapter-install

--- charts/prometheus-adapter/custom.sh
+++ charts/prometheus-adapter/custom.sh
@@ -15,6 +15,15 @@ set -o pipefail
 set -o nounset
 
 #==============================
+image=$(yq '.prometheus-adapter.image.repository' values.yaml)
+repository=$(echo ${image} | cut -d'/' -f 2-)
+yq -i .prometheus-adapter.image.repository=\"$repository\"  values.yaml
+
+yq -i '.prometheus-adapter.image.registry="k8s.m.daocloud.io"'  values.yaml
+yq -i '.prometheus-adapter.prometheus.url="http://insight-agent-kube-prometh-prometheus.insight-system.svc"'  values.yaml
+
+sed -i '/resources: {}/,/^\s*$/{/^\s*$/!s/#/ /g}' values.yaml
+sed -i 's/resources: {}/resources: /g' values.yaml
 
 if [ "$(uname)" == "Darwin" ];then
   sed  -i '' 's?"{{ .Values.image.repository }}:{{ .Values.image.tag }}"?"{{ .Values.image.registry }}/{{ .Values.image.repository }}:{{ .Values.image.tag }}"?'   charts/prometheus-adapter/templates/deployment.yaml
