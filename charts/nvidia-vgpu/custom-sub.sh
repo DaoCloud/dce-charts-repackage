637a351c82006e95aaad71ddbe4eb3e6c22b2109
chaunceyjiang <chaunceyjiang@gmail.com>
2024-02-04 10:38:23 +0800

    Expose the deviceCoreScaling parameter.
    Signed-off-by: chaunceyjiang <chaunceyjiang@gmail.com>

--- charts/nvidia-vgpu/custom-sub.sh
+++ charts/nvidia-vgpu/custom-sub.sh
@@ -21,139 +21,137 @@ echo "custom-sub.sh"
 
 # sed resourceName: "nvidia.com/gpu" to resourceName: "nvidia.com/vgpu"
 if [ $os == "Darwin" ];then
-    sed -i "" "s/resourceName: \"nvidia.com\/gpu\"/resourceName: \"nvidia.com\/vgpu\"/g" charts/vgpu/values.yaml
+    sed -i "" "s/resourceName: \"nvidia.com\/gpu\"/resourceName: \"nvidia.com\/vgpu\"/g" charts/hami/values.yaml
 elif [ $os == "Linux" ];then
-    sed -i "s/resourceName: \"nvidia.com\/gpu\"/resourceName: \"nvidia.com\/vgpu\"/g" charts/vgpu/values.yaml
+    sed -i "s/resourceName: \"nvidia.com\/gpu\"/resourceName: \"nvidia.com\/vgpu\"/g" charts/hami/values.yaml
 fi
 
 
 # sed scheduler.kubeScheduler.registry and scheduler.kubeScheduler.repository
-line=`sed -n -e '/image: registry.cn-hangzhou/=' charts/vgpu/values.yaml`
+line=`sed -n -e '/image: registry.cn-hangzhou/=' charts/hami/values.yaml`
 if [ $os == "Darwin" ];then
-     sed -i "" "$line"d charts/vgpu/values.yaml
+     sed -i "" "$line"d charts/hami/values.yaml
      sed -i "" "$line i\\
     registry: k8s-gcr.m.daocloud.io
-      " charts/vgpu/values.yaml
+      " charts/hami/values.yaml
      sed -i "" "$((line+1)) i\\
     repository: kube-scheduler
-      " charts/vgpu/values.yaml
+      " charts/hami/values.yaml
 elif [ $os == "Linux" ];then
-     sed -i "$line"d charts/vgpu/values.yaml
+     sed -i "$line"d charts/hami/values.yaml
      sed -i "$line i\\
     registry: k8s-gcr.m.daocloud.io
-           " charts/vgpu/values.yaml
+           " charts/hami/values.yaml
      sed -i "$((line+1)) i\\
     repository: kube-scheduler
-           " charts/vgpu/values.yaml
+           " charts/hami/values.yaml
 fi
 
 # set scheduler imageTag v1.20.0 to "v1.24.0"
 if [ $os == "Darwin" ];then
-        sed -i "" "s/imageTag: \"v1.20.0\"/imageTag: \"v1.24.0\"/g" charts/vgpu/values.yaml
+        sed -i "" "s/imageTag: \"v1.20.0\"/imageTag: \"v1.24.0\"/g" charts/hami/values.yaml
 elif [ $os == "Linux" ];then
-        sed -i "s/imageTag: \"v1.20.0\"/imageTag: \"v1.24.0\"/g" charts/vgpu/values.yaml
+        sed -i "s/imageTag: \"v1.20.0\"/imageTag: \"v1.24.0\"/g" charts/hami/values.yaml
 fi
 
 # sed scheduler.extender.image
-line=$(sed -n -e '/ image: "4pdosc\/k8s-vdevice"/=' charts/vgpu/values.yaml  | head -n 1)
+line=$(sed -n -e '/ image: "projecthami\/hami"/=' charts/hami/values.yaml  | head -n 1)
 if [ $os == "Darwin" ];then
-     sed -i "" "$line"d charts/vgpu/values.yaml
+     sed -i "" "$line"d charts/hami/values.yaml
      sed -i "" "$line i\\
     registry: docker.m.daocloud.io
-          " charts/vgpu/values.yaml
+          " charts/hami/values.yaml
      sed -i "" "$((line+1)) i\\
-    repository: 4pdosc/k8s-vdevice
-           " charts/vgpu/values.yaml
+    repository: projecthami/hami
+           " charts/hami/values.yaml
 elif [ $os == "Linux" ];then
-    sed -i "$line"d charts/vgpu/values.yaml
+    sed -i "$line"d charts/hami/values.yaml
     sed -i  "$line i\\
     registry: docker.m.daocloud.io
-            " charts/vgpu/values.yaml
+            " charts/hami/values.yaml
     sed -i  "$((line+1)) i\\
-    repository: 4pdosc/k8s-vdevice
-            " charts/vgpu/values.yaml
+    repository: projecthami/hami
+            " charts/hami/values.yaml
 fi
 
-
-line=$(sed -n -e '/ image: "4pdosc\/k8s-vdevice"/=' charts/vgpu/values.yaml  | head -n 1)
+line=$(sed -n -e '/ image: "projecthami\/hami"/=' charts/hami/values.yaml  | head -n 1)
 if [ $os == "Darwin" ];then
-     sed -i "" "$line"d charts/vgpu/values.yaml
+     sed -i "" "$line"d charts/hami/values.yaml
      sed -i "" "$line i\\
   registry: docker.m.daocloud.io
-          " charts/vgpu/values.yaml
+          " charts/hami/values.yaml
      sed -i "" "$((line+1)) i\\
-  repository: 4pdosc/k8s-vdevice
-           " charts/vgpu/values.yaml
+  repository: projecthami/hami
+           " charts/hami/values.yaml
 elif [ $os == "Linux" ];then
-    sed -i "$line"d charts/vgpu/values.yaml
+    sed -i "$line"d charts/hami/values.yaml
     sed -i  "$line i\\
   registry: docker.m.daocloud.io
-            " charts/vgpu/values.yaml
+            " charts/hami/values.yaml
     sed -i  "$((line+1)) i\\
-  repository: 4pdosc/k8s-vdevice
-            " charts/vgpu/values.yaml
+  repository: projecthami/hami
+            " charts/hami/values.yaml
 fi
-
 # sed patch image
-line=$(sed -n -e '/ image: docker.io\/jettech\/kube-webhook-certgen/=' charts/vgpu/values.yaml  | head -n 1)
+line=$(sed -n -e '/ image: docker.io\/jettech\/kube-webhook-certgen/=' charts/hami/values.yaml  | head -n 1)
 if [ $os == "Darwin" ];then
-     sed -i "" "$line"d charts/vgpu/values.yaml
+     sed -i "" "$line"d charts/hami/values.yaml
      sed -i "" "$line i\\
     registry: docker.m.daocloud.io
-          " charts/vgpu/values.yaml
+          " charts/hami/values.yaml
      sed -i "" "$((line+1)) i\\
     repository: jettech/kube-webhook-certgen
-           " charts/vgpu/values.yaml
+           " charts/hami/values.yaml
      sed -i "" "$((line+2)) i\\
     tag: v1.5.2
-          " charts/vgpu/values.yaml
+          " charts/hami/values.yaml
 elif [ $os == "Linux" ];then
-    sed -i "$line"d charts/vgpu/values.yaml
+    sed -i "$line"d charts/hami/values.yaml
     sed -i  "$line i\\
     registry: docker.m.daocloud.io
-            " charts/vgpu/values.yaml
+            " charts/hami/values.yaml
     sed -i  "$((line+1)) i\\
     repository: jettech/kube-webhook-certgen
-            " charts/vgpu/values.yaml
+            " charts/hami/values.yaml
     sed -i "$((line+2)) i\\
     tag: v1.5.2
-              " charts/vgpu/values.yaml
+              " charts/hami/values.yaml
 fi
 
 # sed patch imageNew
-line=$(sed -n -e '/ imageNew: liangjw\/kube-webhook-certgen/=' charts/vgpu/values.yaml  | head -n 1)
+line=$(sed -n -e '/ imageNew: liangjw\/kube-webhook-certgen/=' charts/hami/values.yaml  | head -n 1)
 if [ $os == "Darwin" ];then
-    sed -i "" "$line"d charts/vgpu/values.yaml
+    sed -i "" "$line"d charts/hami/values.yaml
      sed -i "" "$((line)) i\\
     newRepository: liangjw/kube-webhook-certgen
-           " charts/vgpu/values.yaml
+           " charts/hami/values.yaml
      sed -i "" "$((line+1)) i\\
     newTag: v1.1.1
-          " charts/vgpu/values.yaml
+          " charts/hami/values.yaml
 elif [ $os == "Linux" ];then
-     sed -i "$line"d charts/vgpu/values.yaml
+     sed -i "$line"d charts/hami/values.yaml
      sed -i  "$((line)) i\\
     newRepository: liangjw/kube-webhook-certgen
-           " charts/vgpu/values.yaml
+           " charts/hami/values.yaml
      sed -i  "$((line+1)) i\\
     newTag: v1.1.1
-          " charts/vgpu/values.yaml
+          " charts/hami/values.yaml
 fi
 
 # add device-cores-scaling config key
-line=$(sed -n -e '/deviceMemoryScaling: 1/=' charts/vgpu/values.yaml  | head -n 1)
+line=$(sed -n -e '/deviceMemoryScaling: 1/=' charts/hami/values.yaml  | head -n 1)
 if [ $os == "Darwin" ];then
     sed -i "" "$((line)) i\\
   deviceCoresScaling: 1.0
-    " charts/vgpu/values.yaml
+    " charts/hami/values.yaml
 elif [ $os == "Linux" ];then
     sed -i "$((line)) i\\
   deviceCoresScaling: 1.0
-    " charts/vgpu/values.yaml
+    " charts/hami/values.yaml
 fi
 
-# add resources to values.yaml
-cat >> charts/vgpu/values.yaml << EOL
+# add resources config key
+cat >> charts/hami/values.yaml << EOL
 resources:
   limits:
     cpu: 500m
@@ -162,5 +160,3 @@ resources:
     cpu: 100m
     memory: 128Mi
 EOL
-
-cat charts/vgpu/values.yaml
